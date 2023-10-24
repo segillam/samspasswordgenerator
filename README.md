0688a1f452d94a16210d5d941bc9c4e287e600c3
segillam <gillamsamuel@yahoo.com>
2023-10-24 01:38:13 -0400

    Created js code for the password generator and created a README

---
+++ README.md
@@ -0,0 +1,24 @@
+# Sam's Password Generator
+
+## Description
+
+I created this application as a way to generate new passwords based on criteria that users can choose. This takes away any need to think about a new password when the password you use for everything else doesn't fit the requirements of a new password you may need to create in the future!
+
+
+## Usage
+
+THIS APPLICATION DOES NOT CURRENT FUNCTION AS DESIRED
+
+Users load the page, click the generate button, answer the prompts, and then copy the password that is created. 
+
+![screenshot of application after hitting the generate button](screenshot.png)
+
+In its current state, users experience everything up to the final password creation.
+
+## Credits
+
+I used stackoverflow while troubleshooting the characterOptions array creation
+https://stackoverflow.com/questions/1374126/how-to-extend-an-existing-javascript-array-with-another-array-without-creating
+
+I used stackoverflow while troublshooting prompt() validation methods
+https://stackoverflow.com/questions/23097859/javascript-prompt-validation
--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 // Assignment code here
+function generatePassword() {
+  alert('Hello! Lets build you a passowrd! I just have a few questions first.')
 
+
+  //requests a length for password and asks again if the input is not valid
+  do{
+    var passwordLength = window.prompt('Between 8 and 128 characters, what length should the password be?', "");
+}while(isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8);
+
+
+//each asks if specific type of character should be included
+  var passwordUppercase = prompt('Should I include any uppercase letters? (Type "Y" or "N")');
+  var passwordLowercase = prompt('Should I include any lowercase letters? (Type "Y" or "N")');
+  var passwordNumbers = prompt('Should I include any numbers? (Type "Y" or "N")');
+  var passwordSpecial = prompt('Should I include any special characters? (Type "Y" or "N")');
+
+  //initializes an array for character options
+  let characterOptions = [];
+  //arrays for certain character options
+  let lowerAlphaOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let upperAlphaOptions = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let numericOptions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let specialOptions = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/', '~', '`'];
+
+  //adds each optional character array to the initialized array
+  if (passwordUppercase = "Y"){
+    characterOptions.push(upperAlphaOptions);
+  }
+
+  if (passwordLowercase = "Y"){
+    characterOptions.push(lowerAlphaOptions);
+  }
+
+  if (passwordNumbers = "Y"){
+    characterOptions.push(numericOptions);
+  }
+
+  if (specialOptions = "Y"){
+    characterOptions.push(specialOptions);
+  }
+
+  //generates a password of the required length by choosing random characters from the full character list
+  for (var i = 0; i < passwordLength; i++) {
+    var randomCharacter = Math.floor(Math.random() * characterOptions.length);
+    password[i] = characterOptions[randomCharacter]
+  }
+
+  password = password.join("");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
